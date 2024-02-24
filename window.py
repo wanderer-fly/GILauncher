35025e6a8abbfc85851a2e6e99686470c9aedb77
wanderer-fly <wand.liulang@gmail.com>
2024-02-24 23:41:52 +0800

    update

--- window.py
+++ window.py
@@ -14,7 +14,7 @@ class Launcher(QWidget):
         self.path = self.config.read_config()['game_path']
 
         self.unlockfps_status = self.config.read_config()['unlockfps']
-        # self.exec_cmd = f"prime-run wine {self.get_unlockfps_status()} {self.path}"
+        self.touch_mode = False
 
         self.initUI()
 
@@ -30,25 +30,46 @@ class Launcher(QWidget):
         scaled_pixmap = pixmap.scaled(window_size, Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
 
 
-        # 创建背景标签
+        # background
         background_label = QLabel(self)
         background_label.setPixmap(scaled_pixmap)
         background_label.setAlignment(Qt.AlignCenter)
 
         label = QLabel("原神", self)
         label.setGeometry(100, 100, 300, 200)
-        label.setStyleSheet("font-size: 100pt; font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;")
+        label.setStyleSheet("font-size: 80pt; font-family: “Arial”,”Microsoft YaHei”,”黑体”,”宋体”,sans-serif;")
 
         # file chooser
         chooser = QPushButton('选择原神安装路径', self)
         chooser.clicked.connect(self.showChooser)
+        chooser.setGeometry(910, 730, 200, 30)
         
         # unlockfps
+        ufps_label = QLabel("解鎖刷新率上限", self)
+        ufps_label.setGeometry(730, 620, 200, 20)  # 調整寬度
+        ufps_label.adjustSize()
         unlockfps_box = QCheckBox('Unlock FPS', self)
         unlockfps_box.stateChanged.connect(self.unlockfps_option)
-        unlockfps_box.setGeometry(0, 50, 15, 15)
-        
+        unlockfps_box.setGeometry(840, 620, 15, 15)
 
+        # touch mode
+        touch_label = QLabel("觸摸模式", self)
+        touch_label.setGeometry(730, 650, 200, 20)
+        ufps_label.adjustSize()
+        touch_box = QCheckBox('', self)
+        touch_box.stateChanged.connect(self.touch_option)
+        touch_box.setGeometry(840, 650, 200, 20)
+
+        # game path
+        self.path_label = QLabel(f"當前游戲路徑：{self.path}", self)
+        self.path_label.setGeometry(600, 570, 200, 20)
+        self.path_label.adjustSize()
+
+        # game version (os/cn)
+        self.ver_label = QLabel(f"當前游戲版本：{self.get_game_version()[0]}", self)
+        self.ver_label.setGeometry(940, 690, 250, 20)
+        self.ver_label.adjustSize()
+        
         # launch game
         launch_game = QPushButton('Launch', self)
         launch_game.clicked.connect(self.launch_gi)
@@ -61,7 +82,6 @@ class Launcher(QWidget):
                                   """)
         launch_game.setGeometry(890, 600, 250, 80)
 
-        # self.setLayout(box)
         self.setFixedSize(self.width(), self.height())
 
         self.show()
@@ -69,20 +89,35 @@ class Launcher(QWidget):
     def launch_gi(self):
             print(f'原神，启动！ {self.path}')
             if not self.path is None:
-                exec_cmd = f"prime-run wine {self.get_unlockfps_status()} {self.path}"
-                # os.system(f'prime-run wine "{self.path}"')
+                exec_cmd = f"{self.get_unlockfps_status()} \"{self.path}/{self.get_game_version()[1]}\" {self.get_touch_status()}"
                 print(exec_cmd)
                 os.system(exec_cmd)
             else:
                 self.showDialog("Error", "请先选择游戏可执行文件")
 
     def showChooser(self):
-        fname = QFileDialog.getOpenFileName(self, '选择原神可执行程式', '.', 'Executable Files (*.exe);;All Files (*)')
-
-        if fname[0]:
-            print(fname[0])
-            self.path = fname[0]
+        directory = QFileDialog.getExistingDirectory(self, '选择原神安装路径', '.')
+        if directory:
+            print(directory)
+            self.path = directory
             self.config.set_config('game_path', self.path)
+
+            # 檢查目錄中是否存在 GenshinImpact.exe 或 YuanShen.exe
+            exe_files = ['GenshinImpact.exe', 'YuanShen.exe']
+            exe_found = False
+            for exe_file in exe_files:
+                if os.path.exists(os.path.join(directory, exe_file)):
+                    exe_found = True
+                    break
+
+            # 更新 path_label 的文本
+            if exe_found:
+                self.path_label.setText(f"當前游戲路徑：{self.path}")
+            else:
+                QMessageBox.critical(self, "錯誤", "找不到可执行文件")
+
+            self.path_label.adjustSize()
+
 
     def unlockfps_option(self, state):
         if state == 2:
@@ -95,15 +130,36 @@ class Launcher(QWidget):
     def get_unlockfps_status(self):
         print(self.unlockfps_status)
         if self.unlockfps_status:
-            return "./unlockfps.exe"
+            return "unlockfps.exe"
         else:
             return ""
+        
+    def touch_option(self, state):
+        if state == 2:
+            print("Touch Mode enabled")
+            self.touch_mode = True
+        else:
+            self.touch_mode = False
+
+    def get_touch_status(self):
+        if self.touch_mode:
+            return " use_mobile_platform -is_cloud 1 -platform_type CLOUD_THIRD_PARTY_MOBILE"
+        else:
+            return ""
+        
+    def get_game_version(self): # [version, filename]
+        game_files = ['GenshinImpact.exe', 'YuanShen.exe']
+
+        for game_file in game_files:
+            if os.path.exists(os.path.join(self.path, game_file)):
+                if game_file == 'GenshinImpact.exe':
+                    return ['OS', game_file]
+                elif game_file == 'YuanShen.exe':
+                    return ['CN', game_file]
 
     def showDialog(self, title="Dialog", msg="Message"):
         msg_box = QMessageBox()
-
         msg_box.setWindowTitle(title)
         msg_box.setText(msg)
         msg_box.setIcon(QMessageBox.Information)
-
         msg_box.setStandardButtons(QMessageBox.Ok)
